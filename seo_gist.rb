77a0df127df15bb66da5b380429fd38d3643f79c
rob hurring <robhurring@gmail.com>
2013-10-01 20:59:54 -0400

    wrap noscript code with <pre><code>, closes #2

--- seo_gist.rb
+++ seo_gist.rb
@@ -151,7 +151,7 @@ __END__
     %a{id:'select', href:'#'} Select Embed Code
     %pre#embed
       = escape_html(@gist.script)
-      = preserve "\n\n&lt;noscript>&lt;pre>\n"+escape_html(escape_html(@gist.raw))+"\n&lt;/pre>&lt;/noscript>"
+      = preserve "\n\n&lt;noscript>&lt;pre>&lt;code>\n"+escape_html(escape_html(@gist.raw))+"\n&lt;/code>&lt;/pre>&lt;/noscript>"
     %h2 Gist Preview
     = @gist.script
   - else
